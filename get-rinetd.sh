1e4c4b4f42cf23c6b8a748805bc7b2b95324bdfe
linhua55 <5linhua5@gmail.com>
2017-08-27 15:03:45 +0800

    temp update url for UDP

--- get-rinetd.sh
+++ get-rinetd.sh
@@ -2,7 +2,8 @@
 # Usage:
 #   curl https://raw.githubusercontent.com/linhua55/lkl_study/master/get-rinetd.sh | bash
 
-export RINET_URL="https://github.com/linhua55/lkl_study/releases/download/v1.2/rinetd_bbr_powered"
+# export RINET_URL="https://github.com/linhua55/lkl_study/releases/download/v1.2/rinetd_bbr_powered"
+export RINET_URL="https://drive.google.com/uc?id=0B0D0hDHteoksVzZ4MG5hRkhqYlk"
 
 if [ "$(id -u)" != "0" ]; then
     echo "ERROR: Please run as root"
